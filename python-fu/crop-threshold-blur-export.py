e07e16dfa8654419b044bf5e670be8119e804a84
Sean Budning <seanbud@users.noreply.github.com>
2020-03-16 15:39:17 -0400

    Update crop-threshold-blur-export.py

--- python-fu/crop-threshold-blur-export.py
+++ python-fu/crop-threshold-blur-export.py
@@ -1,47 +1,37 @@
 from gimpfu import *
 
+# Get list of layers
+img = gimp.image_list()[0]
+layers = list(img.layers)
 
-def ExportAll():
-	# Get list of layers
-	img = gimp.image_list()[0]
-	layers = list(img.layers)
-	
-	
-	# Get directory path
-	first_file_name = pdb.gimp_image_get_filename(img)
-	file_dir = first_file_name[0:first_file_name.rfind('\\')] + '\\'
-	
-	# Create folder for shadows
-	import os
-	file_dir = file_dir + '\\shadow_sprites'
-	if not os.path.exists(file_dir):
-		os.makedirs(file_dir)
-		
-		
-	# Adjust layers and export
-	for l in layers:
-		# Save File
-		pdb.gimp_file_save(img, l, file_dir + '\\' + l.name, '?')
 
-def ExportLayer(i):
-	# Get list of layers
-	img = gimp.image_list()[0]
-	layers = list(img.layers)
+# Get directory path
+first_file_name = pdb.gimp_image_get_filename(img)
+file_dir = first_file_name[0:first_file_name.rfind('\\')] + '\\'
+
+# Create folder for shadows
+import os
+file_dir = file_dir + '\\shadow_sprites'
+if not os.path.exists(file_dir):
+	os.makedirs(file_dir)
+
+
+
+# Crop image
+new_size = img.width * 0.6
+offset = img.width/2 - new_size/2
+pdb.gimp_image_crop(img, new_size, new_size, offset, offset)
+
+# Adjust layers and export
+for l in layers:
+	# Layer to black
+	pdb.gimp_drawable_threshold(l, 0, 0, 0)
 	
+	# Blur layer
+	for n in range(15):
+		pdb.plug_in_blur(img, l)
 	
-	# Get directory path
-	first_file_name = pdb.gimp_image_get_filename(img)
-	file_dir = first_file_name[0:first_file_name.rfind('\\')] + '\\'
-	
-	# Create folder for shadows
-	import os
-	file_dir = file_dir + '\\shadow_sprites'
-	if not os.path.exists(file_dir):
-		os.makedirs(file_dir)
-		
-		
-	# Adjust layers and export
-	for l in layers:
-		if int(l.name[0:l.name.rfind('_')]) == i:
-			# Save File
-			pdb.gimp_file_save(img, l, file_dir + '\\' + l.name, '?')
+	# Save File
+	layer_name = l.name.partition('.')[0]
+	newname = layer_name + '_shadow.png'
+	pdb.gimp_file_save(img, l, file_dir + '\\' + newname, '?')
