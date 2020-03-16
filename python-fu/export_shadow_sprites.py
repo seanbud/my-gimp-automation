93b0c08795862a82f6820251a5a77fe85e3677e9
Sean Budning <seanbud@users.noreply.github.com>
2020-03-16 15:39:34 -0400

    Update export_shadow_sprites.py

--- python-fu/export_shadow_sprites.py
+++ python-fu/export_shadow_sprites.py
@@ -1,22 +1,47 @@
 from gimpfu import *
 
-# Get list of layers
-img = gimp.image_list()[0]
-layers = list(img.layers)
 
+def ExportAll():
+	# Get list of layers
+	img = gimp.image_list()[0]
+	layers = list(img.layers)
+	
+	
+	# Get directory path
+	first_file_name = pdb.gimp_image_get_filename(img)
+	file_dir = first_file_name[0:first_file_name.rfind('\\')] + '\\'
+	
+	# Create folder for shadows
+	import os
+	file_dir = file_dir + '\\shadow_sprites'
+	if not os.path.exists(file_dir):
+		os.makedirs(file_dir)
+		
+		
+	# Adjust layers and export
+	for l in layers:
+		# Save File
+		pdb.gimp_file_save(img, l, file_dir + '\\' + l.name, '?')
 
-# Get directory path
-first_file_name = pdb.gimp_image_get_filename(img)
-file_dir = first_file_name[0:first_file_name.rfind('\\')] + '\\'
-
-# Create folder for shadows
-import os
-file_dir = file_dir + '\\shadow_sprites'
-if not os.path.exists(file_dir):
-	os.makedirs(file_dir)
-
-
-# Adjust layers and export
-for l in layers:
-	# Save File
-	pdb.gimp_file_save(img, l, file_dir + '\\' + l.name, '?')
+def ExportLayer(i):
+	# Get list of layers
+	img = gimp.image_list()[0]
+	layers = list(img.layers)
+	
+	
+	# Get directory path
+	first_file_name = pdb.gimp_image_get_filename(img)
+	file_dir = first_file_name[0:first_file_name.rfind('\\')] + '\\'
+	
+	# Create folder for shadows
+	import os
+	file_dir = file_dir + '\\shadow_sprites'
+	if not os.path.exists(file_dir):
+		os.makedirs(file_dir)
+		
+		
+	# Adjust layers and export
+	for l in layers:
+		if int(l.name[0:l.name.rfind('_')]) == i:
+			# Save File
+			pdb.gimp_file_save(img, l, file_dir + '\\' + l.name, '?')
